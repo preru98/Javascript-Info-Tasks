12b5b9fa9f02908f10f7d8484576e706681d2e14
Prerna <prerna1998.ps@gmail.com>
2019-06-04 22:08:24 +0530

    Select all diagnols

---
+++ selectAllDiagnalCells.js
@@ -0,0 +1,7 @@
+let V1=document.querySelector('#C1');
+let V5=document.querySelector('#C5');
+let V9=document.querySelector('#C9');
+V1.style.backgroundColor='red';
+V5.style.backgroundColor='red';
+V9.style.backgroundColor='red';
+
