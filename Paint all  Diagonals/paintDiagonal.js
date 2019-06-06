ef637061bc9e57555b03d37b93c5d455a53f9322
Prerna <prerna1998.ps@gmail.com>
2019-06-06 20:58:32 +0530

    Paint all diagonals

--- Paint all  Diagonals/paintDiagonal.js
+++ Paint all  Diagonals/paintDiagonal.js
@@ -2,7 +2,7 @@
 let tableVar=document.querySelector('#tableID');
 for(let i=0;i<3;i++)
 { console.log(i);
-tableVar.rows[i].cells[i].style.backgroundColor='red';
+  tableVar.rows[i].cells[i].style.backgroundColor='red';
 }
 
 
@@ -10,5 +10,5 @@ tableVar.rows[i].cells[i].style.backgroundColor='red';
 let varTable=document.querySelector('#tableID');
 for(let j=0;j<varTable.rows.length;j++)
 {
-    varTable.rows[j].cells[j].style.backgroundColor='red';
+   varTable.rows[j].cells[j].style.backgroundColor='red';
 }
