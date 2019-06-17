3f7e36c9f1ad4e036144f1b7701457a8b49bc7c3
Prerna <prerna1998.ps@gmail.com>
2019-06-17 20:39:50 +0530

    Hide self

---
+++ Hide Self/hideSelf.js
@@ -0,0 +1,8 @@
+ch=0;
+// let varText=document.querySelector('#para1');
+let varButton=document.querySelector('#button1');
+varButton.addEventListener('click',function(){
+ 
+     varButton.style.visibility='hidden'; 
+  
+});
