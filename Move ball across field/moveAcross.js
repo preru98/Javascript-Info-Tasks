09bfe5c3669403e8ceba6fbb9dfb55dc50c8d60e
Prerna <prerna1998.ps@gmail.com>
2019-06-19 18:25:13 +0530

    Ball center coming exactly under the pointer

--- Move ball across field/moveAcross.js
+++ Move ball across field/moveAcross.js
@@ -2,8 +2,8 @@ let varBody=document.querySelector('body');
 let varImage=document.querySelector('#image1')
 //  alert(varBody);  //doubt 1
 varBody.addEventListener('click',function(event){
-// alert('coordinates' + event.pageX+':'+event.pageY);
-let centreX=event.pageX-40;
+//  alert('coordinates' + event.pageX+':'+event.pageY);
+ let centreX=event.pageX-40;
 let centreY=event.pageY-40;
 let leftVar=centreX+'px'; 
 let topVar=centreY+'px'; 
