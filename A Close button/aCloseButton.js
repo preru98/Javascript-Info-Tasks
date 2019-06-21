5daad0103c8efd77ddf98e1350d63a67fdfd54ea
Prerna <prerna1998.ps@gmail.com>
2019-06-21 20:37:58 +0530

    Generic css for all the containers(image,divText and divButton),no changes in javascript file, dynamic arrangement left

--- A Close button/aCloseButton.js
+++ A Close button/aCloseButton.js
@@ -1,15 +1,27 @@
 let buttonVar1=document.querySelector("#buttonOneImage");
-let divVar1=document.querySelector("#divOne");
-let buttonVar3=document.querySelector("#buttonThreeImage");
-let divVar3=document.querySelector("#divThree");
-buttonVar3.addEventListener('click',function(){
-    divVar3.style.visibility='hidden';
-});
 let buttonVar2=document.querySelector("#buttonTwoImage");
+let buttonVar3=document.querySelector("#buttonThreeImage");
+let flagA=true,flagB=true,flagC=true;       //true value: divs are still visible
+
+let divVar1=document.querySelector("#divOne");
 let divVar2=document.querySelector("#divTwo");
+let divVar3=document.querySelector("#divThree");
+
 buttonVar1.addEventListener('click',function(){
-    divVar1.style.visibility='hidden';
-});
-buttonVar2.addEventListener('click',function(){
-    divVar2.style.visibility='hidden';
+    divVar1.parentNode.removeChild(divVar1);
+    flagA=false;
+    if(flagB==true)
+        divVar2.style.top='4px';
+//     // else
+//     //     if(flagC===true)
+//     //         divVar3.style.top='4px';
     });
+
+// buttonVar2.addEventListener('click',function(){
+//     divVar2.style.visibility='hidden';
+//     });
+
+// buttonVar3.addEventListener('click',function(){
+//     divVar3.style.visibility='hidden';
+//     });
+
