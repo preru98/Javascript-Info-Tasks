5bd2e22e1be3e72cf0cbc1d0de2157aaf20d8056
Prerna <prerna1998.ps@gmail.com>
2019-06-21 17:38:30 +0530

    All divs are working fine, dynamic arrangement left

--- A Close button/aCloseButton.js
+++ A Close button/aCloseButton.js
@@ -1,5 +1,15 @@
-let buttonVar=document.querySelector("#buttonOneImage");
-let divVar=document.querySelector("#divOne");
-buttonVar.addEventListener('click',function(){
-divVar.style.visibility='hidden';
+let buttonVar1=document.querySelector("#buttonOneImage");
+let divVar1=document.querySelector("#divOne");
+let buttonVar3=document.querySelector("#buttonThreeImage");
+let divVar3=document.querySelector("#divThree");
+buttonVar3.addEventListener('click',function(){
+    divVar3.style.visibility='hidden';
 });
+let buttonVar2=document.querySelector("#buttonTwoImage");
+let divVar2=document.querySelector("#divTwo");
+buttonVar1.addEventListener('click',function(){
+    divVar1.style.visibility='hidden';
+});
+buttonVar2.addEventListener('click',function(){
+    divVar2.style.visibility='hidden';
+    });
