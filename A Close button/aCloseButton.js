a592b47957ae5252ce4aa65bca371d86c18b2ac6
Prerna <prerna1998.ps@gmail.com>
2019-06-21 16:49:10 +0530

    Uppermost division working fine

--- A Close button/aCloseButton.js
+++ A Close button/aCloseButton.js
@@ -1,1 +1,5 @@
-!  
+let buttonVar=document.querySelector("#buttonOneImage");
+let divVar=document.querySelector("#divOne");
+buttonVar.addEventListener('click',function(){
+divVar.style.visibility='hidden';
+});
