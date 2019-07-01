3906b5c029aa889d7b4bdd64e92213286a046e52
Prerna <prerna1998.ps@gmail.com>
2019-07-01 18:43:06 +0530

    Divs created using javascript

--- Dynamic divs using classes/dynamicDiv.js
+++ Dynamic divs using classes/dynamicDiv.js
@@ -1,17 +1,24 @@
 class addDiv{
-    constructor(content){
+    constructor(content,heading){
         this.content=content;
+        this.heading=heading;
     }
     addNewRecord(){
         let newContainerDiv=document.createElement("div");
         let newImageDiv=document.createElement("div");
         let newCrossButton=document.createElement("img");
-        
+        let newHeading=document.createTextNode(this.heading)
+        let makeBold=document.createElement("strong");
         let newPara=document.createElement("p");
         let newPhrase=document.createTextNode(this.content);
+        let newLine=document.createElement("br");
         
         newCrossButton.src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Crystal_Clear_action_button_cancel.svg/1024px-Crystal_Clear_action_button_cancel.svg.png";
         newCrossButton.height='100%';
+
+        makeBold.appendChild(newHeading);
+        newPara.appendChild(makeBold);
+        newPara.appendChild(newLine);
         newPara.appendChild(newPhrase);
         newImageDiv.appendChild(newCrossButton);
         newContainerDiv.appendChild(newImageDiv);
@@ -27,7 +34,7 @@ class addDiv{
     }
 }
 
-let myDiv=new addDiv('I am a new Div created dynamically ussing javascript');
+let myDiv=new addDiv('I am a new Div created dynamically ussing javascript','Say Hello');
 myDiv.addNewRecord();
 var allButtons= document.querySelectorAll(".buttonOneImage");
 var allDivs= document.querySelectorAll(".divOne");
