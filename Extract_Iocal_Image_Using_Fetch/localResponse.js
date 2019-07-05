252e8f17a05cc6c0078d824c5a9f8faa7d344e00
Prerna <prerna1998.ps@gmail.com>
2019-07-05 18:22:24 +0530

    Using fetch for json response file using prromises, delayed

--- Extract_Iocal_Image_Using_Fetch/localResponse.js
+++ Extract_Iocal_Image_Using_Fetch/localResponse.js
@@ -1,8 +1,13 @@
-fetch("doggo.jpg")
-    .then(function(response){
-        console.log(response);
-        return response.blob();
-    }).then(function(getResponse){
-        console.log(getResponse);
-        document.querySelector('#Image').src=URL.createObjectURL(getResponse);
-    })
+let delayFunction=function(){
+    fetch("doggo.jpg")
+        .then(function(response){
+            console.log(response);
+            return response.blob();
+        }).then(function(getResponse){
+            console.log(getResponse);
+            document.querySelector('#Image').src=URL.createObjectURL(getResponse);
+        })
+}
+console.log('Image will be here soon!');
+alert('Please Wait');
+setTimeout(delayFunction,5000);
