cdb5015f546b74d0443916f1131a63c6a7475f8b
Prerna <prerna1998.ps@gmail.com>
2019-07-09 22:11:55 +0530

    contribution issue

--- working_with_JSON/jsonWork.js
+++ working_with_JSON/jsonWork.js
@@ -1,4 +1,6 @@
 returnPos();
+let attribution: ' &copy ; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributers';
+    
 let myMap = L.map('mapID').setView([51.505, -0.09], 13);
 async function returnPos(){
     const response= await fetch('https://api.wheretheiss.at/v1/satellites/25544');
