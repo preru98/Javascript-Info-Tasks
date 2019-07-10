2df8cbb089b7b9d9d6947a4964c487e792b7cc9a
Prerna <prerna1998.ps@gmail.com>
2019-07-10 09:01:56 +0530

    map appears successfully

--- working_with_JSON/jsonWork.js
+++ working_with_JSON/jsonWork.js
@@ -1,7 +1,11 @@
 returnPos();
-let attribution: ' &copy ; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributers';
-    
+
+let attribution='&copy ; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributers';
+let tileURL='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 let myMap = L.map('mapID').setView([51.505, -0.09], 13);
+let tiles=L.tileLayer(tileURL, { attribution });
+
+tiles.addTo(myMap);
 async function returnPos(){
     const response= await fetch('https://api.wheretheiss.at/v1/satellites/25544');
     const data=await response.json();
