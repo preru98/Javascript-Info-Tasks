61d3d10a6c5be878212e5e55256bfa1775d9afde
Prerna <prerna1998.ps@gmail.com>
2019-07-10 10:17:53 +0530

    map appears successfully,real time coordinates of satellite,marker

--- working_with_JSON/jsonWork.js
+++ working_with_JSON/jsonWork.js
@@ -1,18 +1,25 @@
-returnPos();
+// returnPos();
+const attribution='&copy ; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributers';
+const tileURL='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-let attribution='&copy ; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributers';
-let tileURL='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-let myMap = L.map('mapID').setView([51.505, -0.09], 13);
-let tiles=L.tileLayer(tileURL, { attribution });
+var Icon = L.icon({
+    iconUrl: '200px-International_Space_Station.svg.png',
+    iconSize: [70, 52],
+    iconAnchor: [35, 26],
+});
 
+const myMap = L.map('mapID').setView([51.505, -0.09], 2);
+const marker=L.marker([0,0],{icon:Icon}).addTo(myMap);
+const tiles=L.tileLayer(tileURL, { attribution });
 tiles.addTo(myMap);
 async function returnPos(){
     const response= await fetch('https://api.wheretheiss.at/v1/satellites/25544');
     const data=await response.json();
     console.log(data);
     const{longitude,latitude}=data;
+    marker.setLatLng([latitude,longitude]);
     document.getElementById('Longitude').textContent=longitude;
     document.getElementById('Latitude').textContent=latitude;
 }
 
-//setInterval(returnPos,1500);
+setInterval(returnPos,1000);
