e67e87c0cccbcb135c26fd41be38f521962d6398
Prerna <prerna1998.ps@gmail.com>
2020-04-25 20:30:30 +0530

    Working with froms

--- Using_html_forms_JS/usingHTMLFormsWithJs.js
+++ Using_html_forms_JS/usingHTMLFormsWithJs.js
@@ -1,6 +1,6 @@
 
 let buttonVar=document.getElementById('button1');
-buttonVar.addEventListener('click',function(){
+    buttonVar.addEventListener('click',function(){
     alertMessage();
 });
 function alertMessage(){
