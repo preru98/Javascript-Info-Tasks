a8e6e94705d185f5c617ebf51e36fd6affd0232e
Prerna <prerna1998.ps@gmail.com>
2020-09-07 20:54:05 +0530

    Get user Webcam feed

---
+++ getUserMedia/test.js
@@ -0,0 +1,25 @@
+let videoFeed = document.querySelector('#camera-feed');
+let stopButton = document.querySelector('#stop');
+let startButton = document.querySelector('#start');
+
+stopButton.addEventListener('click', () =>{
+    let stream = videoFeed.srcObject;
+    let tracks = stream.getTracks();
+    console.log(tracks);
+    for(let i=0 ; i <tracks.length; i++){
+        console.log(tracks[i]);
+        tracks[i].stop();
+    }
+});
+
+startButton.addEventListener('click', () =>{
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function (stream) {
+                videoFeed.srcObject = stream;
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+});
